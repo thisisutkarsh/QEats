16eb995dbb2ca8675dd591294b0d6d0392d92b4c
Anand Vaidya <anand1711@gmail.com>
2020-06-02 17:08:03 +0550

    first commit

---
+++ qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
@@ -0,0 +1,22 @@
+
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class Restaurant {
+
+}
+
