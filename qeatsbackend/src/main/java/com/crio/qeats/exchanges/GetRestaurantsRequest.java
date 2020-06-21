d8717e5ff3e10c4e133579f3f88f34258eb3765a
Anand Vaidya <anand1711@gmail.com>
2020-06-21 22:47:32 +0550

    updated changes - 2020-06-21 22:27:32

---
+++ qeatsbackend/src/main/java/com/crio/qeats/exchanges/GetRestaurantsRequest.java
@@ -0,0 +1,23 @@
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.exchanges;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GetRestaurantsRequest {
+
+
+
+}
+
