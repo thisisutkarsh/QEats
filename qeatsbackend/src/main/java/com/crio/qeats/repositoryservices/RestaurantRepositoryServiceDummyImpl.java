3d938a095a612125f24838ae3571ab5c371c85cb
Anand Vaidya <anand1711@gmail.com>
2020-07-06 19:25:36 +0550

    updated changes - 2020-07-06 19:05:36

--- qeatsbackend/src/main/java/com/crio/qeats/repositoryservices/RestaurantRepositoryServiceDummyImpl.java
+++ qeatsbackend/src/main/java/com/crio/qeats/repositoryservices/RestaurantRepositoryServiceDummyImpl.java
@@ -14,10 +14,6 @@ import java.util.concurrent.ThreadLocalRandom;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-
-// CRIO_UNCOMMENT_ONLY_START_MODULE_RESTAURANTSAPI
-// @Service
-// CRIO_UNCOMMENT_ONLY_END_MODULE_RESTAURANTSAPI
 public class RestaurantRepositoryServiceDummyImpl implements RestaurantRepositoryService {
   private static final String FIXTURES = "fixtures/exchanges";
   private ObjectMapper objectMapper = new ObjectMapper();
