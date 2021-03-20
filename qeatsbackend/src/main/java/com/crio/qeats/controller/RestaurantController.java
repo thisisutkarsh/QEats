0f94f8516485ab17b2289fb12ef6ef75e6056cb4
Anand Vaidya <anand1711@gmail.com>
2021-03-21 00:03:55 +0550

    updated changes - 2021-03-20 23:43:55

---
+++ qeatsbackend/src/main/java/com/crio/qeats/controller/RestaurantController.java
@@ -0,0 +1,69 @@
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.controller;
+
+import com.crio.qeats.exchanges.GetRestaurantsRequest;
+import com.crio.qeats.exchanges.GetRestaurantsResponse;
+import com.crio.qeats.services.RestaurantService;
+import java.time.LocalTime;
+import javax.validation.Valid;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class RestaurantController {
+
+  public static final String RESTAURANT_API_ENDPOINT = "/qeats/v1";
+  public static final String RESTAURANTS_API = "/restaurants";
+  public static final String MENU_API = "/menu";
+  public static final String CART_API = "/cart";
+  public static final String CART_ITEM_API = "/cart/item";
+  public static final String CART_CLEAR_API = "/cart/clear";
+  public static final String POST_ORDER_API = "/order";
+  public static final String GET_ORDERS_API = "/orders";
+
+  @Autowired
+  private RestaurantService restaurantService;
+
+
+
+  @GetMapping(RESTAURANTS_API)
+  public ResponseEntity<GetRestaurantsResponse> getRestaurants(
+       GetRestaurantsRequest getRestaurantsRequest) {
+
+    log.info("getRestaurants called with {}", getRestaurantsRequest);
+    GetRestaurantsResponse getRestaurantsResponse;
+
+      getRestaurantsResponse = restaurantService
+          .findAllRestaurantsCloseBy(getRestaurantsRequest, LocalTime.now());
+      log.info("getRestaurants returned {}", getRestaurantsResponse);
+
+    return ResponseEntity.ok().body(getRestaurantsResponse);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
